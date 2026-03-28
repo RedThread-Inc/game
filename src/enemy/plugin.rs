193c87566c50f77f7a883d97411c309ee4997999
Rémi Ménard <menard.remi56330@gmail.com>
2026-03-28 16:47:45 +0100

    [DRAFT] Player attack system
    # Conflicts:
    #	src/enemy/plugin.rs
    #	src/enemy/spawn.rs
    #	src/player/plugin.rs

--- src/enemy/plugin.rs
+++ src/enemy/plugin.rs
@@ -1,4 +1,5 @@
 use crate::enemy::animate::animate_enemies_system;
+use crate::enemy::death::enemy_death_system; // ajouter
 use crate::enemy::movement::move_enemy_towards_player_system;
 use crate::enemy::spawn::spawn_enemies;
 use crate::exceptions::log_rtg_exception;
@@ -9,9 +10,13 @@ pub(crate) struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::InGame), spawn_enemies)
-            .add_systems(Update, (animate_enemies_system.pipe(log_rtg_exception), move_enemy_towards_player_system.pipe(log_rtg_exception))
-                .run_if(in_state(GameState::InGame))
-                .run_if(in_state(InGameState::Playing)));
+        app.add_systems(Startup, spawn_enemies)
+            .add_systems(Update, (
+                animate_enemies_system.pipe(log_rtg_exception),
+                move_enemy_towards_player_system.pipe(log_rtg_exception),
+                enemy_death_system,
+            )
+            .run_if(in_state(GameState::InGame))
+            .run_if(in_state(InGameState::Playing)));
     }
 }
