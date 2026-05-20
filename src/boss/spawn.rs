a53b37c3b22fea25df605f69491c6efaa47e3a2b
Rémi Ménard <menard.remi56330@gmail.com>
2026-05-20 14:52:28 +0200

    Fix boss spawning

--- src/boss/spawn.rs
+++ src/boss/spawn.rs
@@ -14,11 +14,7 @@ pub(crate) fn spawn_boss(
     round: Res<RoundState>,
 ) {
 
-    /*if round.current == 0 || round.current % 5 != 0 {
-        return;
-    }*/
-
-    if round.current != 1 {
+    if round.current == 0 || round.current % 5 != 0 {
         return;
     }
 
