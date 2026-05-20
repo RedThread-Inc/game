16cbd5ae74a46406d1f00bd37ca699171f489922
NaimSdn <saadoune.naim.pro@gmail.com>
2026-05-20 11:24:55 +0200

    Updating | Can shoot the boss now

--- src/boss/mod.rs
+++ src/boss/mod.rs
@@ -14,7 +14,7 @@ pub(crate) const BOSS_ANIM_DT: f32 = 0.1;
 pub(crate) const BOSS_SCALE: f32 = 2.5;
 
 pub(crate) const BOSS_MOVE_SPEED: f32 = 45.0;
-pub(crate) const BOSS_BASE_HP: f32 = 500.0;
+pub(crate) const BOSS_BASE_HP: f32 = 1.0;
 pub(crate) const BOSS_BASE_PROJECTILE_COUNT: u32 = 8;
 pub(crate) const BOSS_BASE_PROJECTILE_SPEED: f32 = 120.0;
 pub(crate) const BOSS_BASE_FIRE_RATE: f32 = 2.0;
