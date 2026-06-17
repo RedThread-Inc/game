e385880fc74baf0862ddc4218bc310d710dc3405
NaimSdn <saadoune.naim.pro@gmail.com>
2026-06-17 14:40:57 +0200

    Updating boss/death and plugin | Adding BossDiedEvent correctly

--- src/boss/plugin.rs
+++ src/boss/plugin.rs
@@ -5,7 +5,6 @@ use crate::boss::damage::{boss_contact_damage_system, boss_projectile_damage_sys
 use crate::boss::death::{BossDiedEvent, boss_death_system};
 use crate::boss::movement::boss_movement_system;
 use crate::boss::spawn::spawn_boss;
-use crate::enemy::spawn::spawn_enemies;
 use crate::exceptions::log_rtg_exception;
 use crate::round::RoundStartedEvent;
 use crate::{GameState, InGameState};
@@ -15,26 +14,27 @@ pub(crate) struct BossPlugin;
 
 impl Plugin for BossPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
-            Update,
-            spawn_boss
-                .run_if(on_message::<RoundStartedEvent>)
-                .run_if(in_state(GameState::InGame))
-                .run_if(in_state(InGameState::Playing)),
-        )
-        .add_systems(
-            Update,
-            (
-                boss_movement_system.pipe(log_rtg_exception),
-                boss_attack_system.pipe(log_rtg_exception),
-                move_boss_projectiles_system,
-                cleanup_boss_projectiles_system,
-                boss_contact_damage_system,
-                boss_projectile_damage_system,
-                boss_death_system,
+        app.add_message::<BossDiedEvent>()
+            .add_systems(
+                Update,
+                spawn_boss
+                    .run_if(on_message::<RoundStartedEvent>)
+                    .run_if(in_state(GameState::InGame))
+                    .run_if(in_state(InGameState::Playing)),
             )
-                .run_if(in_state(GameState::InGame))
-                .run_if(in_state(InGameState::Playing)),
-        );
+            .add_systems(
+                Update,
+                (
+                    boss_movement_system.pipe(log_rtg_exception),
+                    boss_attack_system.pipe(log_rtg_exception),
+                    move_boss_projectiles_system,
+                    cleanup_boss_projectiles_system,
+                    boss_contact_damage_system,
+                    boss_projectile_damage_system,
+                    boss_death_system,
+                )
+                    .run_if(in_state(GameState::InGame))
+                    .run_if(in_state(InGameState::Playing)),
+            );
     }
 }
